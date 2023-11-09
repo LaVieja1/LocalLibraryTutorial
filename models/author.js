7a322f7ad6ffe19f9bab2c334dca8cde2ff565bb
santicano <canosantiago404@gmail.com>
2023-11-09 17:09:01 -0300

    añadido formato de fecha simplificado usando luxon

--- models/author.js
+++ models/author.js
@@ -1,3 +1,4 @@
+const { DateTime } = require("luxon");
 const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
@@ -25,6 +26,18 @@ AuthorSchema.virtual("name").get(function () {
 AuthorSchema.virtual("url").get(function () {
   // We don't use an arrow function as we'll need the this object
   return `/catalog/author/${this._id}`;
+});
+
+// Virtual for author's URL
+AuthorSchema.virtual("date_of_birth_formatted").get(function () {
+  // We don't use an arrow function as we'll need the this object
+  return this.date_of_birth ? DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED) : '';
+});
+
+// Virtual for author's URL
+AuthorSchema.virtual("date_of_death_formatted").get(function () {
+  // We don't use an arrow function as we'll need the this object
+  return this.date_of_death ? DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED) : '';
 });
 
 // Export model
